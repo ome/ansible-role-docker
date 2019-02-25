1a357640bbf8e569307fbbe15adb6fc81d6d000f
Sebastien Besson <seb.besson@gmail.com>
2019-02-25 21:23:50 +0000

    Convert Molecule 1 to Molecule 2

--- molecule/default/tests/test_docker-inactive.py
+++ molecule/default/tests/test_docker-inactive.py
@@ -1,11 +1,13 @@
+import os
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('docker-inactive')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('docker-inactive')
 
 
-def test_docker_installed(Command, Sudo):
-    with Sudo():
-        cmd = Command('docker info')
+def test_docker_installed(host):
+    with host.sudo():
+        cmd = host.command('docker info')
     assert cmd.rc == 1
     assert cmd.stderr.startswith('Cannot connect to the Docker daemon')
